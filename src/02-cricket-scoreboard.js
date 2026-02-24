3df4cfd915d7b56c5db4d16847f0d554213cf4fc
Mehtab Hussain <tradercoder786@gmail.com>
2026-02-24 10:45:51 +0530

    Implement revenue calculation for chaiTapri and enhance cricketScoreboard functionality

--- src/02-cricket-scoreboard.js
+++ src/02-cricket-scoreboard.js
@@ -31,5 +31,25 @@
  *   // => { totalRuns: 0, totalBalls: 0, wickets: 0, fours: 0, sixes: 0 }
  */
 export function cricketScoreboard(balls) {
-  // Your code here
+  if (!Array.isArray(balls) || balls.length === 0) return { totalRuns: 0, totalBalls: 0, wickets: 0, fours: 0, sixes: 0 };
+  let obj = { totalRuns: 0, totalBalls: 0, wickets: 0, fours: 0, sixes: 0 };
+  for (let i = 0; i < balls.length; i++) {
+
+    let ball = balls[i];
+    if (obj['wickets'] >= 10) break;
+
+    obj['totalBalls'] = obj['totalBalls'] + 1;
+
+    if (ball >= 1 && ball <= 6) {
+      if (ball === 4) obj['fours'] = obj['fours'] + 1;
+      else if (ball === 6) obj['sixes'] = obj['sixes'] + 1;
+
+      obj['totalRuns'] = obj['totalRuns'] + ball;
+
+    } else if (ball === -1) {
+      obj['wickets'] = obj['wickets'] + 1;
+    } else if (ball === 0) continue;
+
+  }
+  return obj;
 }
